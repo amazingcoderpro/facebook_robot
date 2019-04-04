c6e0a9945100633f94ac16039e1b6e1c56f4c547
Silence-python <1447385994@qq.com>
2019-04-04 16:37:54 +0800

    异常处理

---
+++ scripts/mobile_exception.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Charles on 19-3-15
+
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class FacebookException(BaseException):
+    """
+    :action: 0 检查首页是否存在, 1 是否记住密码页面, 2 忽略电话号码, 3 忽略上传头像, 4 忽略app下载
+    """
+    MAP_EXP_PROCESSOR = {0: {'key_word': 'div[id="MComposer"]', 'check': 'is_home', 'process': 'process_home'},
+                         1: {'key_word': 'button[type="submit"]', 'check': 'check_remember_password', 'process': 'process_add_password'},
+                         2: {'key_word': 'div[data-sigil="mChromeHeaderRight"]', 'check': 'check_save_telnumber', 'process': 'process_img_verify'},
+                         3: {'key_word': 'div[data-sigil="mChromeHeaderRight"]', 'check': 'check_add_imge', 'process': 'process_tel_verify'},
+                         4: {'key_word': 'div[data-sigil="mChromeHeaderRight"]', 'check': 'check_load_app', 'process': 'process_load_app'},
+                         5: {'key_word': 'button[id="checkpointSubmitButton-actual-button"]', 'check': 'check_verification', 'process': 'process_verification'}
+                         }
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+        self.exception_type = -1
+
+    def auto_process(self, retry=1):
+        while retry > 0:
+            self.check()
+
+            # 如果已经在home页面，不用处理
+            if self.exception_type == 0:
+                return True
+
+            processor = self.MAP_EXP_PROCESSOR.get(self.exception_type, {}).get('process', '')
+            if processor:
+                if hasattr(self, processor):
+                    ret = getattr(self, processor)()
+            else:
+                ret = False
+
+            # 如果无法处理，不用再重试
+            if not ret:
+                return False
+            retry -= 1
+        return True
+
+    def check(self):
+        # 检查点
+        self.exception_type = -1
+        for k, v in self.MAP_EXP_PROCESSOR.items():
+            check_func = v.get('check', '')
+            if check_func and hasattr(self, check_func):
+                if getattr(self, check_func)():
+                    self.exception_type = k
+                    break
+        return self.exception_type
+
+    def check_is_home(self):
+        # 检查是否是首页
+        try:
+            WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(0)['key_word'])))
+        except:
+            return False
+        return True
+
+    def check_remember_password(self):
+        # 记住账号密码
+        try:
+            WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(1)['key_word'])))
+        except:
+            return False
+        return True
+
+    def check_save_telnumber(self):
+        # 首次登录电话号码验证略过
+        try:
+            WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(2)['key_word'])))
+        except:
+            return False
+        return True
+
+    def check_add_imge(self):
+        # 上传头像
+        try:
+            WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(3)['key_word'])))
+        except:
+            return False
+        return True
+
+    def check_load_app(self):
+        # 忽略下载ap也页面
+        try:
+            WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(4)['key_word'])))
+        except:
+            return False
+        return True
+
+    def check_verification(self):
+        # 好友验证页面
+        try:
+            WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(5)['key_word'])))
+        except:
+            return False
+        return True
+
+    def process_home(self):
+        # 处理是否在HOME页面
+        processor = self.MAP_EXP_PROCESSOR.get(self.exception_type)
+        if not processor:
+            print("未检测到Home页面")
+            return False
+
+        if hasattr(self, self.processor):
+            return getattr(self, processor)()
+        return False
+
+    def process_add_password(self):
+        # 记住账号密码
+        try:
+            no_password = WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(1)['key_word'])))
+            no_password.click()
+        except:
+            return False
+        return True
+
+    def process_tel_verify(self):
+        # 忽略电话号码
+        try:
+            tel_number = WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(2)['key_word'])))
+            tel_number.click()
+        except:
+            return False
+        return True
+
+    def process_img_verify(self):
+        # 忽略上传头像
+        try:
+            tel_number = WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(3)['key_word'])))
+            tel_number.click()
+        except:
+            return False
+        return True
+
+    def process_load_app(self):
+        # 上传头像
+        time.sleep(3)
+        try:
+            never_save_number = WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(4)['key_word'])))
+            never_save_number.click()
+        except:
+            return False
+        return True
+
+    def process_verification(self):
+        # 处理好友验证
+        try:
+            never_save_number = WebDriverWait(self.driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.MAP_EXP_PROCESSOR.get(5)['key_word'])))
+            never_save_number.click()
+        except:
+            return False
+        return True
+
+
+
+
+# if __name__ == '__main__':
+#     fb = FacebookException(None)
+#     fb.check()
+#     fb.auto_process(6)
+
+
---
+++ scripts/pc_auto_feed.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Charles on 19-3-15
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from PIL import Image
+from selenium.webdriver.support.ui import Select
+from mobile_exception import FacebookException
+
+
+def auto_feed_mobile(inputs):
+    """
+    养号任务实现
+    :param account: Account类的实例，其中包含了账号相关的属性信息
+    :param mode: 养号模式，0-代表自动模式，程序将会根据账号历史操作情况自动决定养号流程， 1-仅浏览 2-发状态 3-随机点赞 4-随机聊天
+    :return: （code, result) code: 0-代表失败，1-代表成功， result： 处理结果描述
+    """
+    # 登录FB
+    account_num = inputs['account']
+    password = inputs['password']
+    print('script runing:{},{}'.format(account_num, password))
+
+    # 定制浏览器启动项
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--profile-directory=Default')
+    # chrome_options.add_argument("--incognito")
+    chrome_options.add_argument('lang=zh_CN.UTF-8')
+    chrome_options.add_argument("--disable-plugins-discovery")
+    chrome_options.add_argument("--start-maximized")
+
+    # 移动设备仿真
+    mobile_emulation = {
+        'deviceName': 'iPhone 5',
+        # "deviceMetrics": {"width": 600, "height":800, "pixelRatio": 4.0},
+        # "userAgent": "Mozilla/5.0 (Linux; Android 8.0.0; XT1635-02 Build/OPNS27.76-12-22-9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36"
+    }
+    chrome_options = Options()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.delete_all_cookies()
+    driver.get('https://www.facebook.com/')
+    driver.implicitly_wait(10)
+
+    try:
+        # FB登录
+        email_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'email')))
+        email_box.send_keys(account_num)
+        time.sleep(3)
+        password_box = driver.find_element_by_name('pass')
+        password_box.send_keys(password)
+        # driver.get_screenshot_as_file('login.png')
+        time.sleep(3)
+        # login_img = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'email')))
+        # print(login_img)
+        login_btn = driver.find_element_by_css_selector('button[type="button"]')
+        print(login_btn)
+        login_btn.click()
+        # driver.implicitly_wait(10)
+        time.sleep(2)
+
+        # 检查页面是否存在
+        fbexcept = FacebookException(driver)
+        # fbexcept.check()
+        # a = 1
+        if not fbexcept.auto_process():
+            return False
+        # 检查是否在首页面
+        try:
+            WebDriverWait(driver, 6).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'div[id="MComposer"]')))
+        except:
+            return False
+
+        def browse_page():
+            time.sleep(5)
+            driver.execute_script("window.scrollTo(0,600)")
+            time.sleep(4)
+            driver.execute_script("window.scrollTo(0,1200)")
+            time.sleep(3)
+            driver.execute_script("window.scrollTo(1200,0)")
+            time.sleep(3)
+        # 首次登录页面取消弹窗
+        # driver.switch_to_alert().accept()  # 接受警告（等于点了个确定）
+        # message = driver.switch_to_alert().text
+        # print(message)  # 得到弹窗的文本消息,比如得到：请输入用户名！'
+        # driver.switch_to_alert().dismiss()  # 不接受警告（等于点了个取消）
+
+        # 用户行为 查看最新动态
+        time.sleep(3)
+        user_news = driver.find_element_by_css_selector('div[id="bookmarks_jewel"]')
+        user_news.click()
+        browse_page()
+        driver.find_element_by_css_selector('span[data-sigil="most_recent_bookmark"]').click()
+        browse_page()
+
+        time.sleep(3)
+        user_news = driver.find_element_by_css_selector('div[id="bookmarks_jewel"]')
+        user_news.click()
+        browse_page()
+        driver.get("https://m.facebook.com/local_surface/?query_type=HOME&ref=bookmarks")
+        browse_page()
+
+        # 搜索添加朋友
+        time.sleep(3)
+        driver.get("https://m.facebook.com/search/people/?q={}&source=filter&isTrending=0".format("xiaoning"))
+        time.sleep(1000)
+
+        add_search_fridens = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#u_1e_z > div > div')))
+        add_search_fridens.cliick()
+        add__fridens = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, 'a[data-sigil="touchable ajaxify"]')))
+        add__fridens.click()
+
+
+
+
+        # driver.get("https://m.facebook.com/home.php?soft=search")
+        # add_search_fridens = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[data-sigil="search-small-box"]')))
+        # add_search_fridens.send_keys("kkk")
+        # time.sleep(3)
+        # search_fridens = WebDriverWait(driver, 10).until(
+        #     EC.presence_of_element_located((By.CSS_SELECTOR, 'input[data-sigil="search-small-box"]')))
+        # search_fridens.click()
+        #
+        # add_fridens = WebDriverWait(driver, 10).until(
+        #     EC.presence_of_element_located((By.CSS_SELECTOR, 'div[id="u_1v_10"]')))
+        # add_fridens.click()
+
+        # 添加好友
+        # # driver.get('https://m.facebook.com/friends/center/requests/?mff_nav=1#!/friends/center/requests/?rfj&no_hist=1')
+        # driver.find_element_by_css_selector('div[id="requests_jewel"]')
+        # driver.implicitly_wait(10)
+        # driver.find_element_by_css_selector('input[data-sigil="search-small-box"]').send_keys("mengxiaoning")
+        # driver.implicitly_wait(10)
+
+        # 账号被封杀标志
+        # driver.implicitly_wait(10)
+        # driver.find_element_by_css_selector('button[id="checkpointSubmitButton-actual-button"]')
+
+        # add_friends = driver.find_element_by_css_selector('a[data-sigil^="friends-center-nav-item"]')
+        # add_friends.click()
+        # driver.implicitly_wait(10)
+        # time.sleep(5)
+
+    except Exception:
+        driver.delete_all_cookies()
+        driver.quit()
+        # 登录账号记住密码
+        # no_password = driver.find_element_by_css_selector('button[type="submit"]')
+        # driver.implicitly_wait(10)
+        # if no_password:
+        #     pass
+        #     # continue
+        # else:
+        #     print("没有找到记住账号密码的节点")
+
+        # def capath_intercepty():
+        #     driver.get_screenshot_as_file('screenshot.png')
+        #     # 获取验证码位置
+        #     element = driver.find_element_by_css_selector('#captcha > img')
+        #     left = int(element.location['x'])
+        #     top = int(element.location['y'])
+        #     right = int(element.location['x'] + element.size['width'])
+        #     bottom = int(element.location['y'] + element.size['height'])
+        #
+        #     # 通过Image处理验证码图像
+        #     im = Image.open('screenshot.png')
+        #     im = im.crop((left, top, right, bottom))
+        #     im.save('code.png')
+        #
+        # def never_save_password():
+        #     # 登录之后选择不保存密码
+        #     no_password = driver.find_element_by_css_selector('a[name="login"]')
+        #     no_password.click()
+
+
+if __name__ == '__main__':
+    auto_feed_mobile(inputs={'account': "bigeckc@aol.com", 'password': "paddy1875"})
