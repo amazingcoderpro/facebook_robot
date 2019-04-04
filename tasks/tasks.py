0c04f2be9999e980a5c61b67b2896b9f4ba26fe2
Wu Charles <wcadaydayup@163.com>
2019-04-04 10:15:04 +0800

    任务模板

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -16,13 +16,8 @@ class BaseTask(Task):
         logger.error('celery task on_failed, task_id={}, args={}, kwargs={}, exception={}, exception info={}. '
                      .format(task_id, args, kwargs, exc, einfo))
 
-        # self.update_state(task_id=task_id, state='failed', meta={'exc': exc, 'einfo': einfo, 'args': args, 'kwargs': kwargs, 'tt':123})
-        # JobOpt.set_job_by_track_id(task_id, status='failed', result=str(exec), traceback=str(einfo))
-
     def on_success(self, retval, task_id, args, kwargs):
         logger.info('celery task on_success, task_id={}, retval={}. '.format(task_id, retval))
-        # self.update_state(task_id=task_id, state='succeed', meta={'retval': retval, 'args': args, 'kwargs':kwargs, 'tt':456})
-        # JobOpt.set_job_by_track_id(task_id, status='succeed', result=str(retval))
 
 
 # 处理beat的定时任务, 暂不用
@@ -32,35 +27,49 @@ class BaseTask(Task):
 #     app.send_task('tasks.tasks.fb_auto_feed', args=(inputs,),
 #                   queue='feed_account', routing_key='for_feed_account')
 
+TaskResult = {
+    'status': 'failed',        # 'failed', 'succeed'
+    'err_msg': '',
+    'account_status': 'valid',  # valid, invalid, verifying
+    'last_post': '',
+    'last_login': '',
+    'last_chat': '',
+    'last_farming': '',
+    'last_comment': '',
+    'last_edit': '',
+    'phone_number': '',
+    'profile_path': ''
+}
 
-@app.task(base=BaseTask, bind=True, max_retries=1, time_limit=300)
+
+@app.task(base=BaseTask, bind=True, max_retries=1, time_limit=1200)
 def fb_auto_feed(self, inputs):
     logger.info('fb_auto_feed task running')
 
-    # time.sleep(3)
-    # 更新任务状态为running
-    # self.update_state(state="running")
-
-    # do something here
-    time.sleep(60)
-
-    a = random.randint(1, 100)
-    if a % 3 == 1:
-        return {'status': 'failed', 'err_msg': 'auto feed 3-1 failed'}
-    if a % 2 == 0:
-        logger.exception('fb_auto_feed')
-        a = a/0
-
-    return {'status': 'succeed', 'err_msg': 'auto feed good.'}
-
     try:
-        # 执行任务
+        # 分步执行任务
+        # do something here
+        time.sleep(60)
+
+        a = random.randint(1, 100)
+        if a % 3 == 1:
+            TaskResult['status'] = 'failed'
+            TaskResult['err_msg'] = 'a % 3 == 1'
+            return TaskResult
+
+        if a % 2 == 0:
+            logger.exception('fb_auto_feed')
+            a = a / 0
+            return TaskResult
+
         res = scripts.auto_feed(inputs)
     except Exception as e:
         logger.exception('fb_auto_feed catch exception.')
-        self.retry(countdown=10 ** self.request.retries)
+        # self.retry(countdown=10 ** self.request.retries)
+        TaskResult['status'] = 'failed'
+        TaskResult['err_msg'] = str(e)
 
-    return 1
+    return TaskResult
 
 
 @app.task(base=BaseTask, bind=True, max_retries=1, time_limit=300)
