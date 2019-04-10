396551539f70db8419139011b1038c7ef578a6af
Wu Charles <wcadaydayup@163.com>
2019-04-10 14:57:54 +0800

    worker环境测试代码

--- scripts/test_env.py
+++ scripts/test_env.py
@@ -6,11 +6,14 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(900, 800))
+display.start()
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-extensions')
-options.add_argument('--disable-gpu')
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-extensions')
+# options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.google.com/")
 time.sleep(2)
@@ -19,3 +22,4 @@ driver.find_element_by_xpath("./*//input[@name='q']").send_keys(Keys.ENTER)
 time.sleep(4)
 print(driver.current_url)
 print(driver.page_source)
+driver.quit()
