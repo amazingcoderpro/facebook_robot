aba0ced39cdd4a5877d71efcaa23218618e147eb
Silence-python <1447385994@qq.com>
2019-04-17 20:32:21 +0800

    验证按钮异常处理

--- scripts/facebook/mobile_auto_feed.py
+++ scripts/facebook/mobile_auto_feed.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from scripts.facebook.mobile_exception import FacebookException
 from config import logger
 from scripts.utils import super_click
