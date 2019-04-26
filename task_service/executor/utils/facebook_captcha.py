76df73b538703f1d3935c9151b4934e628bcf792
shaowei <shaowei@shouxin168.com>
2019-04-26 16:42:37 +0800

    rebot verify

--- task_service/executor/utils/facebook_captcha.py
+++ task_service/executor/utils/facebook_captcha.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from config import get_support_args
+from config import logger, get_support_args
 import time
 from urllib import parse
 import requests
@@ -12,7 +12,7 @@ captcha_arg = get_support_args()
 class CaptchaVerify:
 
     key_str = "data-sitekey"
-    element_id = "recaptcha-demo"
+    element_class = "div[class='g-recaptcha']"
     current_url = "https://www.facebook.com/checkpoint/block/"
     captcha_in_api = None
     captcha_res_api = None
@@ -33,8 +33,8 @@ class CaptchaVerify:
         机器人验证，获取页面的特定字符串，向2captcha的API发送两次请求，最终将获取到的字符串提交到页面提交。
         """
         try:
-            ele = self.driver.find_element_by_id()
-            value = ele.get_attribute(CaptchaVerify.element_id)
+            ele = self.driver.find_element_by_css_selector(self.element_class)
+            value = ele.get_attribute(CaptchaVerify.key_str)
             captcha_id = CaptchaVerify.get_captcha_id(value)
             captcha_str = CaptchaVerify.get_captcha_str(captcha_id)
             if not captcha_str:
@@ -55,12 +55,14 @@ class CaptchaVerify:
         """
         parameter = {
             "key": cls.captcha_api_key,
-            "googlekey": cls.captcha_api_key,
+            "googlekey": value,
             "pageurl": cls.captcha_in_api,
             "method": "userrecaptcha"
         }
         url = cls.captcha_in_api + "?" + parse.urlencode(parameter)
+        logger.info(u"机器人验证: 第一次请求url-->{},参数-->{}".format(url, parameter))
         result = requests.get(url)
+        logger.info(u"机器人验证: 第一次请求结果-->{}".format(result.text))
         return result.text.split('|')[1]
 
     @classmethod
@@ -77,14 +79,17 @@ class CaptchaVerify:
             "id": captcha_id
         }
         url = cls.captcha_res_api + "?" + parse.urlencode(parameter)
+        logger.info(u"机器人验证: 第二次请求url-->{},参数-->{}".format(url,parameter))
         time.sleep(90)
         result = requests.get(url)
         count = 0
         while "CAPCHA_NOT_READY" in result.text and count < 20:
+            logger.info(u"机器人验证: 第二次请求结果-->{}<--重试{}次".format(result.text,count))
             result = requests.get(url)
             time.sleep(5)
             count += 1
         if "ERROR_CAPTCHA_UNSOLVABLE" in result.text:
+            logger.info(u"机器人验证: 第二次请求------未解决----{}".format(result.text))
             return False
         return result.text.split('|')[1]
 
