b2eb474d97fe39e010dc4d13025d9418fbe72d94
shaowei <shaowei@shouxin168.com>
2019-04-29 18:05:42 +0800

    pc feed

--- task_service/executor/facebook/pc_actions.py
+++ task_service/executor/facebook/pc_actions.py
@@ -48,7 +48,7 @@ class FacebookPCActions(FacebookActions):
                 self.driver.get(self.start_url)
 
                 WebDriverWait(self.driver, 6).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, 'div[id="MComposer"]')))
+                    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[data-testid="search_input"]')))
                 logger.info("login success by cookies！ account={}".format(self.account))
                 return True, 0
         except Exception as e:
@@ -79,7 +79,6 @@ class FacebookPCActions(FacebookActions):
             logger.exception("login by cookies failed. continue use password, account={}, e={}".format(self.account, e))
             return self.fb_exp.auto_process(4, wait=2)
 
-
     def browse_home(self):
         """
         首页内容浏览
@@ -106,6 +105,7 @@ class FacebookPCActions(FacebookActions):
         try:
             limit = 1 if limit <= 0 else limit
             logger.info('增加好友: friends={}, limit={}'.format(search_keys, limit))
+            result = {item: 0 for item in search_keys}
             for friend in search_keys:
                 page_url = "https://www.facebook.com/search/people/?q={}&epa=SERP_TAB".format(friend)
                 self.driver.get(page_url)
@@ -115,7 +115,7 @@ class FacebookPCActions(FacebookActions):
                     EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='FriendButton'")))
 
                 # 找到新的好友列表
-                self.browse_home(self.driver, browse_times=5, distance=0, interval=5, back_top=True)
+                self.browse_page(browse_times=5, distance=0, interval=5, back_top=True)
                 new_friends = self.driver.find_elements_by_css_selector("button[aria-label='Add Friend'")
                 if not new_friends:
                     logger.warning('增加好友: can not find any friend. friend keyword={}'.format(friend))
@@ -136,7 +136,8 @@ class FacebookPCActions(FacebookActions):
                     finally:
                         self.click(idx, self.driver)
                         time.sleep(3)
-            logger.info("增加好友: add friend succeed.")
+                        result[friend] += 1
+            logger.info("增加好友: 增加结果为-->{}".format(result))
             self.driver.get(self.start_url)
             time.sleep(5)
             return True, 0
@@ -229,7 +230,7 @@ class FacebookPCActions(FacebookActions):
         """
 
         try:
-            logger.info("发送状态功能: sentence={},".format(contents,))
+            logger.info("发送状态功能: contents={},".format(contents,))
             message_url = "https://www.facebook.com/home.php?sk=h_chr&ref=bookmarks"
             self.driver.get(message_url)
 
@@ -256,7 +257,8 @@ class FacebookPCActions(FacebookActions):
                         try:
                             if "What's on your mind" in row.text:
                                 try:
-                                    self.send_keys(row,contents)
+                                    row.send_keys(contents)
+                                    # self.send_keys(row,contents)
                                 except Exception as e:
                                     continue
                                 else:
@@ -320,6 +322,44 @@ class FacebookPCActions(FacebookActions):
             logger.exception("user_home browsing failed  error ={}".format(e))
             return self.fb_exp.auto_process(3)
 
+    def browse_page(self, browse_times=0, distance=0, interval=0, back_top=True):
+        """
+        浏览页面
+        :param driver: 浏览器驱动
+        :param browse_times: 浏览次数
+        :param distance: 每次间隔距离，默认为零，代表使用随机距离
+        :param interval: 间隔时间， 单位秒, 默认为零，代表使用随机停顿时间
+        :param back_top: 是否回到顶点
+        :return:
+        """
+        # 浏览页面js
+        try:
+            logger.info('browse_page start.')
+            y_dis = 0
+            if browse_times <= 0:
+                browse_times = random.randint(3, 15)
+
+            for i in range(browse_times):
+                if interval <= 0:
+                    time.sleep(random.randint(1, 10))
+                else:
+                    time.sleep(interval)
+
+                if distance > 0:
+                    y_dis += distance
+                else:
+                    y_dis += random.randint(20, 200)
+
+                self.driver.execute_script("window.scrollTo(0,{})".format(y_dis))
+
+            if back_top:
+                self.driver.execute_script("window.scrollTo(0,0)")
+            return True
+        except Exception as e:
+            logger.exception('browse_page exception. e={}'.format(e))
+            return self.fb_exp.auto_process(3)
+
+
 
 # if __name__ == '__main__':
     # filename = '../../resource/facebook_account.txt'
@@ -345,14 +385,27 @@ class FacebookPCActions(FacebookActions):
     #         fma.browse_user_center(3)
     #         time.sleep(100)
 
+
 if __name__ == '__main__':
     user_account = str(17610069110)
     user_password = str("sanmang111..fb").strip()
 
-    # 登陆
-    fma = FacebookPCActions(account_info={"account": user_account, "password": user_password}, finger_print={"user_agent": ""}, headless=False)
+    # 初始化
+    cookies=[{'domain': '.facebook.com', 'httpOnly': False, 'name': 'presence', 'path': '/', 'secure': True, 'value': 'EDvF3EtimeF1556521496EuserFA21B35381807782A2EstateFDt3F_5b_5dG556521496536CEchFDp_5f1B35381807782F2CC'}, {'domain': '.facebook.com', 'expiry': 1564297494.289986, 'httpOnly': True, 'name': 'fr', 'path': '/', 'secure': True, 'value': '14Cgytn5XS9ADvrMS.AWWAmG4Z8RUATBEoDXsVDoRqB1g.BcxqHO.Is.FzG.0.0.BcxqIW.AWVueXRy'}, {'domain': '.facebook.com', 'expiry': 1564297485.612012, 'httpOnly': True, 'name': 'xs', 'path': '/', 'secure': True, 'value': '31%3AzbNHAuA7vvOB-Q%3A2%3A1556521482%3A-1%3A-1'}, {'domain': '.facebook.com', 'expiry': 1557126281, 'httpOnly': False, 'name': 'dpr', 'path': '/', 'secure': True, 'value': '2'}, {'domain': '.facebook.com', 'expiry': 1619593469.742837, 'httpOnly': True, 'name': 'datr', 'path': '/', 'secure': True, 'value': 'zqHGXKNTrxN8ELHDhr2hXAEo'}, {'domain': '.facebook.com', 'expiry': 1564297485.611967, 'httpOnly': False, 'name': 'c_user', 'path': '/', 'secure': True, 'value': '100035381807782'}, {'domain': '.facebook.com', 'expiry': 1556611487.566037, 'httpOnly': True, 'name': 'spin', 'path': '/', 'secure': True, 'value': 'r.1000651590_b.trunk_t.1556521485_s.1_v.2_'}, {'domain': '.facebook.com', 'expiry': 1557126293, 'httpOnly': False, 'name': 'wd', 'path': '/', 'secure': True, 'value': '1200x754'}, {'domain': '.facebook.com', 'expiry': 1619593485.611922, 'httpOnly': True, 'name': 'sb', 'path': '/', 'secure': True, 'value': 'zqHGXDSVQ0-0sXJTMh0cHnju'}]
+
+    fma = FacebookPCActions(account_info={"account": user_account, "password": user_password, "cookies": cookies}, finger_print={"user_agent": ""}, headless=False)
     if not fma.start_chrome():
         print("start chrome failed")
     fma.set_exception_processor(
         FacebookExceptionProcessor(fma.driver, env="pc", account=fma.account, gender=fma.gender))
+    # 登陆
     res, status = fma.login()
+
+    # 浏览页面
+    #fma.browse_home()
+    # 增加好友
+    #fma.add_friends(["pig","dog"], 2)
+    # 发送状态
+    #fma.post_status("wo shi yi zhi xiaoxiaoniao 鸟")
+    # 
+
